38e3c202fa93446d9138c3749e36b8503cb68b53
rajmaurya95 <gautamshakya9528@gmail.com>
2023-09-12 15:03:36 +0530

    Updates

---
+++ precache-manifest.a7cccacad50a2145ef6611875565594f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6062fd371a485d169838",
+    "url": "/news_more/static/css/main.62d02ed7.chunk.css"
+  },
+  {
+    "revision": "6062fd371a485d169838",
+    "url": "/news_more/static/js/main.6062fd37.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/news_more/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "3338378a9bbd02a53eed",
+    "url": "/news_more/static/js/2.3338378a.chunk.js"
+  },
+  {
+    "revision": "68b804febafde9fffbc3",
+    "url": "/news_more/static/js/runtime~main.68b804fe.js"
+  },
+  {
+    "revision": "a47f2018fb7a169103d9ecfe431e7c8d",
+    "url": "/news_more/static/media/spinner.a47f2018.gif"
+  },
+  {
+    "revision": "e03d03a1a7936c7eb23ccccc7016f0fd",
+    "url": "/news_more/index.html"
+  }
+];
